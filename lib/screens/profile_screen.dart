8b44a17a81bbc7cc751383626a36822a9a6fea48
고범석 <kobums23@gmail.com>
2022-05-27 11:30:26 +0900

    profile_screen 추가

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -37,22 +37,165 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Home',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          TextButton(
-            child: const Text("logout"),
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      children: [
+        ProfilePhoto(
+          onClicked: () {},
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        Column(
+          children: const [
+            Text(
+              "Name",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            Text(
+              "name@gmail.com",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ],
+        ),
+        const SizedBox(height: 24),
+        Center(
+          child: ElevatedButton(
             onPressed: () {
-              showAertDialog(context);
+              print('프로필 변경');
             },
+            style: ElevatedButton.styleFrom(
+              shape: const StadiumBorder(),
+              onPrimary: Colors.white,
+            ),
+            child: const Text('프로필 변경'),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            BuildButton(context, '1500', 'Picture'),
+            BuildDivider(),
+            BuildButton(context, '135', 'Following'),
+            BuildDivider(),
+            BuildButton(context, '150', 'Followers'),
+          ],
+        ),
+        const SizedBox(height: 48),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 48),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'About',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Text(
+                "Perhaps the feelings that we experience when we are in love represent a normal state. Being in love shows a person who he should be. Perhaps the feelings that we experience when we are in love represent a normal state. Being in love shows a person who he should be.",
+                style: TextStyle(fontSize: 16, height: 1.4),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget BuildDivider() {
+    return const SizedBox(
+      height: 24,
+      child: VerticalDivider(),
+    );
+  }
+}
+
+Widget BuildButton(BuildContext context, String num, String text) {
+  return MaterialButton(
+    padding: const EdgeInsets.symmetric(vertical: 4),
+    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+    onPressed: () {},
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          num,
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        const SizedBox(height: 2),
+        Text(
+          text,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ],
+    ),
+  );
+}
+
+class ProfilePhoto extends StatelessWidget {
+  final VoidCallback onClicked;
+  const ProfilePhoto({Key? key, required this.onClicked}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          ClipOval(
+            child: Material(
+              color: Colors.transparent,
+              child: Ink.image(
+                image: const NetworkImage(
+                  'https://images.unsplash.com/photo-1653256464939-d13e40b6089c?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587',
+                ),
+                fit: BoxFit.cover,
+                width: 128,
+                height: 128,
+                child: InkWell(onTap: onClicked),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 4,
+            child: ClipOval(
+              child: Container(
+                padding: const EdgeInsets.all(3),
+                color: Colors.white,
+                child: ClipOval(
+                  child: Container(
+                    padding: const EdgeInsets.all(8),
+                    color: Colors.blue,
+                    child: const Icon(
+                      Icons.edit,
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
 }
+
+// Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           Text(
+//             'Home',
+//             style: Theme.of(context).textTheme.headline4,
+//           ),
+//           TextButton(
+//             child: const Text("logout"),
+//             onPressed: () {
+//               showAertDialog(context);
+//             },
+//           ),
+//         ],
+//       ),
